5249feb31018028dc87bde4bb49fbbf66595d1cd
forrestjgq <mr.jianggq@gmail.com>
2017-12-09 18:55:40 +0800

    add wait test

--- proc/fork.c
+++ proc/fork.c
@@ -3,6 +3,7 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
@@ -11,9 +12,22 @@
 #include <string.h>
 #include "dbg.h"
 
-
+void fexit1(void) {
+    DBG("fexit1");
+}
+void fexit2(void) {
+    DBG("fexit2");
+}
+void fexit3(void) {
+    DBG("fexit3");
+}
+void fexit4(int v, void *p){
+    DBG("fexit4 %d on %s", v, (char *)p);
+}
+char *str_arg = "forrest jiang";
 int main(int argc, char *argv[]) {
 
+    int ret_st =0;
     char *str;
     int len;
 
@@ -24,27 +38,70 @@ int main(int argc, char *argv[]) {
     int ret = fork();
     if(ret == -1) {
         FAIL("Exec fail");
+
     } else if (ret == 0) {
-        DBG("Parent");
-        if(lseek(fd, 100, SEEK_SET) == (off_t)-1)
-            FAIL("Parent Seek");
-        str = "forrest";
-        len = strlen(str);
-        if(len != write(fd, str, len))
-            FAIL("Parent write");
-    } else {
-        DBG("Child");
+        ret_st = 10;
+        DBG("====> child pid %jd ppid %jd", (intmax_t)getpid(), (intmax_t)getppid());
         if(lseek(fd, 10, SEEK_SET) == (off_t)-1)
             FAIL("Child Seek");
         str = "jiang";
         len = strlen(str);
         if(len != write(fd, str, len))
             FAIL("Child write");
+        sleep(10);
+        DBG("Child exit");
+#if 0 // for core dump test
+        char *s = NULL;
+        *s = 1;
+#endif
+
+    } else {
+        DBG("====> parent pid %jd ppid %jd", (intmax_t)getpid(), (intmax_t)getppid());
+        if(lseek(fd, 100, SEEK_SET) == (off_t)-1)
+            FAIL("Parent Seek");
+        str = "forrest";
+        len = strlen(str);
+        if(len != write(fd, str, len))
+            FAIL("Parent write");
+
+        /**
+         * will be called in the reverse order of registering
+         */
+        if(atexit(fexit1) != 0)
+            FAIL("Reg 1");
+        if(atexit(fexit2) != 0)
+            FAIL("Reg 2");
+        if(on_exit(fexit4, str_arg) != 0)
+            FAIL("Reg 4");
+        if(atexit(fexit3) != 0)
+            FAIL("Reg 3");
+
+
+        int st;
+        pid_t pid = wait(&st);
+        DBG("wait fork pid %d st %d", pid, st);
+        if(ret != pid)
+            DBG("Child ret pid %d mismatch", ret);
+
+        if(WIFEXITED(st)) {
+            DBG("Child exit normally with status %d", WEXITSTATUS(st));
+        }
+        else if(WIFSIGNALED(st)) {
+            DBG("Child exit by signal with sig %d%s",
+                    WTERMSIG(st),
+                    WCOREDUMP(st) ? ", core dumped" : "");
+        }
+        else if(WIFSTOPPED(st)) {
+            DBG("Child exit stopped by sig %d", WSTOPSIG(st));
+        }
+        else if(WIFCONTINUED(st)) {
+            DBG("Child exit continued");
+        }
+
     }
 
-    PERR("====> pid %jd ppid %jd", (intmax_t)getpid(), (intmax_t)getppid());
 
 
     close(fd);
-    return 0;
+    return ret_st;
 }
--- proc/test.sh
+++ proc/test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+target="all"
+if [ $# -eq 1 ]; then
+    target=$1
+fi
+
+echo "target = $target"
 clean() 
 {
     for cfile in *.c
@@ -9,71 +15,167 @@ clean()
     done
 }
 
-for cfile in *.c
-do
-    obj=${cfile%.c}
-    gcc -Wall -I../include $cfile -o $obj
-    if [ $? -ne 0 ]; then
-        echo "$cfile compiling failed"
-        clean
-        exit 2
-    fi
-done
+compile() {
+    for cfile in *.c
+    do
+        obj=${cfile%.c}
+        gcc -Wall -I../include $cfile -o $obj
+        if [ $? -ne 0 ]; then
+            echo "$cfile compiling failed"
+            clean
+            exit 2
+        fi
+    done
+}
+
+t_fork() {
+
+    echo ""
+    echo "Start test fork.c"
+    echo "======================================="
+    cp fork.c test.txt
+    ./fork
+    head -c 120 test.txt
+    echo ""
+    echo "fork done"
+    rm test.txt
+
+}
+
+t_id() {
+
+    echo ""
+    echo "Start test id.c"
+    echo "======================================="
+    echo "Please input 0 to 9 to start, q to end"
+    ./id | ./id | ./id | ./id
+
+    echo ""
+    echo "id done"
+}
+
+t_execl() {
+
+    echo ""
+    echo "Start test execl.c"
+    echo "======================================="
+    echo "Please input 0 to 9 to start, q to end"
+    ./execl ./id id_execl
+    echo ""
+    echo "id done"
+}
 
 
-echo ""
-echo "Start test fork.c"
-echo "======================================="
-cp fork.c test.txt
-./fork
-sleep 1
-cat test.txt
-rm test.txt
+t_execle() {
+
+    echo ""
+    echo "Start test execle.c"
+    echo "======================================="
+    ./execle
 
 
-echo ""
-echo "Start test id.c"
-echo "======================================="
-echo "Please input 0 to 9 to start, q to end"
-./id | ./id | ./id | ./id
+    echo ""
+    echo "execle done"
+}
 
-echo ""
-echo "Start test execl.c"
-echo "======================================="
-echo "Please input 0 to 9 to start, q to end"
-./execl ./id id_execl
 
-echo ""
-echo "Start test execle.c"
-echo "======================================="
-./execle
+t_execlp() {
 
-echo ""
-echo "Start test execlp.c"
-echo "======================================="
-./execlp vim vi_bin
+    echo ""
+    echo "Start test execlp.c"
+    echo "======================================="
+    ./execlp vim vi_bin
 
-echo ""
-echo "Start test execv.c"
-echo "======================================="
-./execv /usr/bin/vim vi_bin execv.c
 
-echo ""
-echo "Start test execvp.c"
-echo "======================================="
-./execvp vim vi_bin execvp.c
+    echo ""
+    echo "execlp done"
+}
 
-echo ""
-echo "Start test execve.c"
-echo "======================================="
-./execve
 
-echo ""
-echo "Start test exec.c"
-echo "======================================="
-./exec 0
+t_execv() {
+
+    echo ""
+    echo "Start test execv.c"
+    echo "======================================="
+    ./execv /usr/bin/vim vi_bin execv.c
+
+
+    echo ""
+    echo "execv done"
+}
+
+
+t_execvp() {
+
+    echo ""
+    echo "Start test execvp.c"
+    echo "======================================="
+    ./execvp vim vi_bin execvp.c
+
+
+    echo ""
+    echo "execvp done"
+}
+
+
+t_execve() {
+
+    echo ""
+    echo "Start test execve.c"
+    echo "======================================="
+    ./execve
+
+
+    echo ""
+    echo "execve done"
+}
+
+
+t_exec() {
+
+    echo ""
+    echo "Start test exec.c"
+    echo "======================================="
+    ./exec 0
+
+    echo ""
+    echo "exec done"
+}
+
+
+compile
+case $target in
+    all)
+        t_fork
+        t_id
+        t_execle
+        t_execl
+        t_execlp
+        t_execv
+        t_execvp
+        t_execve
+        t_exec
+        ;;
+
+    fork)  t_fork;;
+    id) t_id;;
+    execle) t_execle;;
+    execl) t_execl;;
+    execlp) t_execlp;;
+    execv) t_execv;;
+    execvp) t_execvp;;
+    execve) t_execve;;
+    exec) t_exec;;
+    *)
+        echo "invalid target $target"
+        exit 3
+esac
 
 echo ""
 echo ""
 echo "Done"
+
 clean
+
+
+
