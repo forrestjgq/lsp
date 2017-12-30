6d41216b1f3d40578a90bcdcf1cb0560ab0d807a
forrestjgq <mr.jianggq@gmail.com>
2017-12-30 11:37:14 +0800

    upd pthread merge sh

--- thread/pwpt/merge.sh
+++ thread/pwpt/merge.sh
@@ -8,7 +8,7 @@ cnt=0
 while [ $cnt -le 8 ]
 do
     echo "process pthread$cnt.md"
-    cat pthread$cnt.md sed -r 's/^(#+ )(([0-9]{0,2}\.)*([0-9]{1,2}))( .*)$/\<span id=\"\2\"\> \<\/span\>\n&/' | sed -r 's/([sS]ection )([0-9.]+)/\[\1\2\]\(#\2\)/g' > pthread.md
+    cat pthread$cnt.md | sed -r 's/^(#+ )(([0-9]{0,2}\.)*([0-9]{1,2}))( .*)$/\<span id=\"\2\"\> \<\/span\>\n&/' | sed -r 's/([sS]ection )([0-9.]+)/\[\1\2\]\(#\2\)/g' >> pthread.md
     let "cnt=cnt+1"
 done
 
